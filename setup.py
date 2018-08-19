9445414604af7ad24f9a3ea582c73149a58b6f01
tnantoka <tnantoka@bornneet.com>
2018-08-19 00:12:16 +0000

    release

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
+
 
 setup(
     name='norilog',
     version='1.0.0',
-    package=find_packages(),
+    long_description=read_file('README.rst'),
+    packages=find_packages(),
+    author='tnantoka',
+    author_email='tnantoka@bornneet.com',
+    url='https://github.com/tnantoka/norilog',
     include_package_data=True,
     install_requires=[
         'Flask'
